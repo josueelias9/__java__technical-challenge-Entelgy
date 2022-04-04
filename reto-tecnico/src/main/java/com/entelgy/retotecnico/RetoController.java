fefd2f3b1102ad23227e704a37cf6eb950a0b992
Josué <josueeliashuaman@gmail.com>
2022-04-03 23:40:00 -0500

    habilitacion de archivo externo "properties"

--- reto-tecnico/src/main/java/com/entelgy/retotecnico/RetoController.java
+++ reto-tecnico/src/main/java/com/entelgy/retotecnico/RetoController.java
@@ -1,5 +1,7 @@
 package com.entelgy.retotecnico;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,17 @@ class EmployeeController {
     // Aggregate root
     // tag::get-aggregate-root[]
 
+    @Value("%{mi.direccion}")
+    private String direccion;
+
     @GetMapping("/hola")
     String hola() {
+ 
+        ConsumirURL consum = new ConsumirURL();
+        JSONObject hola = consum.facade(direccion);
+        System.out.println(hola.toString(4));
         return "hola manooo";
-    }
+   }
 
     @PostMapping("/hola")
     String newEmployee(@RequestBody String mi_input) {
